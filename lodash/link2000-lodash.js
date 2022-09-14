b45ee8cd2cd9396a3c1d2179429be31a29be5866
lilong <2875673600@qq.com>
2022-09-14 21:32:54 +0800

    add

--- lodash/link2000-lodash.js
+++ lodash/link2000-lodash.js
@@ -1,5 +1,28 @@
 var link2000 = {
-  chunk: function() {},
+  chunk: function (array, size = 1) {
+
+    var ary = []
+    var ary1 = []
+
+    if (size == 0) {
+      return array
+    }
+    if (size >= array.length && size !== 0) {
+      return array
+    }
+
+    for (var i = 0; i < array.length; i++) {
+      ary.push(array[i])
+      if (ary.length == size) {
+        ary1.push(ary)
+        ary = []
+      }
+    }
+    if (ary[0]) {
+      ary1.push(ary)
+    }
+    return ary1
+  },
 
   compact: function() {},
 
